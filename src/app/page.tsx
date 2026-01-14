917abb7df562a6a3ce8819035758df1cfa498755
Quentin Erriquez <quentinx07@gmail.com>
2026-01-14 15:58:13 +0100

    calculatrice classique

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,114 @@
 import { useRef } from "react";
 
 export default function Home() {
-  const refResult = useRef<HTMLTextAreaElement>(null);
-  const addPerformanceEntry = async (formData: FormData) => {
-    const entry = formData.get("entry");
-    console.log("New performance entry:", entry);
-    refResult.current!.value += `${entry}\n`;
-  };
+  const touches = [1, 2, 3, 4, 5, 6, 7, 8, 9, `+/-`, 0, `.`];
+  const operators = [`+`, `-`, `*`, `/`, `=`];
+  const outputRef = useRef<HTMLTextAreaElement>(null);
 
+  const updateInput = (value: string | number) => {
+    if (outputRef.current) {
+      if (value === "+/-") {
+        const currentValue = outputRef.current.value;
+        if (currentValue.startsWith("-")) {
+          outputRef.current.value = currentValue.slice(1);
+        } else {
+          outputRef.current.value = "-" + currentValue;
+        }
+        return;
+      }
+      outputRef.current.value += value;
+    }
+  };
+  const calculate = () => {
+    if (outputRef.current) {
+      if (outputRef.current.value.trim() === "") {
+        showAlert("Error: Empty Expression");
+        return;
+      }
+      try {
+        const result = eval(outputRef.current.value);
+        outputRef.current.value = result;
+      } catch (error) {
+        showAlert("Error: Invalid Expression");
+      }
+    }
+  };
+  const deleteLast = () => {
+    if (outputRef.current) {
+      outputRef.current!.value = outputRef.current!.value.slice(0, -1);
+      if (outputRef.current!.value === "") {
+        outputRef.current!.placeholder = "";
+      }
+    }
+  };
+  const deleteAll = () => {
+    if (outputRef.current) {
+      outputRef.current.value = "";
+      outputRef.current.placeholder = "";
+    }
+  };
+  const showAlert = (alert: string) => {
+    if (outputRef.current) {
+      outputRef.current.value = "";
+      outputRef.current.placeholder = alert;
+    }
+  };
   return (
-    <div className="flex h-full items-center justify-between flex-col overflow-hidden">
-      <h1 className="flex text-4xl font-bold w-full text-center mt-6 underline justify-center">
-        Performance Tracker
-      </h1>
-      <div className="flex-1 width-constrained border relative rounded-lg shadow-lg border-gray-300 p-2 ">
-        <textarea
-          ref={refResult}
-          disabled
-          className="resize-none w-full h-32 p-2 border border-gray-300 rounded-lg"
-          value={``}
-        />
-        <form action={addPerformanceEntry} className="flex flex-col gap-4">
-          <input
-            type="text"
-            name="entry"
-            id="entry"
-            className="border w-full"
-          />
-          <input type="submit" value="Submit" className="border bg-blue-400" />
-        </form>
+    <div className="flex h-lvh items-center justify-center flex-col overflow-hidden dark:bg-black dark:text-white">
+      <div className="width-constrained h-[90%] border flex flex-col items-center">
+        <div className="flex-1 p-2 w-full">
+          {/* visible output */}
+          <textarea
+            ref={outputRef}
+            disabled
+            name="output"
+            id="output"
+            className="w-full h-full bg-white/10 p-2 rounded resize-none border border-gray-300 dark:bg-black dark:border-gray-600"
+            defaultValue={``}
+            placeholder="Try me !"
+          ></textarea>
+        </div>
+        {/* buttons */}
+        <div className="w-full flex-2 p-3 flex items-center border-t border-gray-300 dark:border-gray-600">
+          <div className="h-full flex-3 grid grid-cols-3 gap-2 p-2">
+            <button onClick={() => deleteLast()} className="circle-button-red">
+              {`<x`}
+            </button>
+            <button onClick={() => deleteAll()} className="circle-button-red">
+              {`C`}
+            </button>
+            <button
+              onClick={() => updateInput(`%`)}
+              className="circle-button-red"
+            >
+              {`%`}
+            </button>
+            {touches.map((touch) => (
+              <button
+                key={touch}
+                onClick={() => updateInput(touch)}
+                className="circle-button-white"
+              >
+                {touch}
+              </button>
+            ))}
+          </div>
+          <div className="h-full flex-1 grid grid-cols-1 gap-2 p-2">
+            {operators.map((operator) => (
+              <button
+                key={operator}
+                onClick={
+                  operator == `=`
+                    ? () => calculate()
+                    : () => updateInput(operator)
+                }
+                className="circle-button-blue"
+              >
+                {operator}
+              </button>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/logic/fonctions.ts
@@ -0,0 +1,14 @@
+export const factorial = (n: number): number => {
+  if (n < 0) {
+    throw new Error("Factorial is not defined for negative numbers");
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+};
+
