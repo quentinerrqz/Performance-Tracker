773d96a8f264b5518223f36eb311df1dd97beb03
Quentin Erriquez <quentinx07@gmail.com>
2026-01-15 11:37:40 +0100

    move speedInsights into body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,11 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <SpeedInsights />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen overflow-hidden`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
